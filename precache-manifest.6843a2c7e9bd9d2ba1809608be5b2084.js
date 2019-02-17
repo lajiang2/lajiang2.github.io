9119a3df90a3ec6babc930269e44668c1b0bee4a
lawrence <lawrence@airbears2-10-142-133-91.airbears2.1918.berkeley.edu>
2019-02-17 12:41:29 -0800

    Updates

---
+++ precache-manifest.6843a2c7e9bd9d2ba1809608be5b2084.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a01140ffd89db6a46365",
+    "url": "/static/js/main.a01140ff.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "a01140ffd89db6a46365",
+    "url": "/static/css/main.7633072e.chunk.css"
+  },
+  {
+    "revision": "87e447c07bc0b4dcb3f79f7033f708fe",
+    "url": "/index.html"
+  }
+];
