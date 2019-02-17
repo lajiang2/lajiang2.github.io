aed3bb2e47b62ed64a0f06c3bd4df9aa34e9addf
lawrence <lawrence@airbears2-10-142-131-87.airbears2.1918.berkeley.edu>
2019-02-17 11:35:09 -0800

    Updates

---
+++ precache-manifest.394f98b81a3c7a5d4ec82042b0aab5b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lajiang2.github.io/personal-website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6c60f8617fc8d69d83c8",
+    "url": "/lajiang2.github.io/personal-website/static/js/runtime~main.6c60f861.js"
+  },
+  {
+    "revision": "168ba7c5368570e9bcdd",
+    "url": "/lajiang2.github.io/personal-website/static/js/main.168ba7c5.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/lajiang2.github.io/personal-website/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "168ba7c5368570e9bcdd",
+    "url": "/lajiang2.github.io/personal-website/static/css/main.7633072e.chunk.css"
+  },
+  {
+    "revision": "1a7bbed4af151d6f5227475015d347a9",
+    "url": "/lajiang2.github.io/personal-website/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/lajiang2.github.io/personal-website/precache-manifest.394f98b81a3c7a5d4ec82042b0aab5b6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/lajiang2.github.io/personal-website/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
